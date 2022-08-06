01336a458885930198e736c93fde74b872310441
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-08-06 15:20:49 +0300

    implement removing todos and move(up/down)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React, {useCallback, useState} from 'react';
+import React, { useState} from 'react';
 import { TodoList } from './components/TodoList/TodoList';
 import {Todo} from "./components/todo.typedefs";
 import {AddForm} from "./components/AddForm/AddForm";
+
+const ShiftByType = {
+  'up': -1,
+  'down': 1,
+}
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -16,10 +21,33 @@ function App() {
     setTodos((todos) => [...todos]);
   };
 
+  const move = (place: Todo[], type: keyof typeof ShiftByType) => (index: number) => {
+    const shift = ShiftByType[type];
+
+    [place[index], place[index + shift]] = [place[index + shift], place[index]];
+
+    setTodos((todos) => [...todos]);
+  }
+
+  const removeTodo = (place: Todo[]) => (id: number) => {
+    const itemToRemoveIndex = place.findIndex((item) => id === item.id);
+
+    if (itemToRemoveIndex !== -1) {
+      place.splice(itemToRemoveIndex, 1);
+
+      setTodos((todos) => [...todos]);
+    }
+  };
+
   return (
     <div className="App">
       <AddForm addTodo={addTodo(todos)} />
-      <TodoList items={todos} addTodo={addTodo} />
+      <TodoList
+        items={todos}
+        addTodo={addTodo}
+        removeTodo={removeTodo}
+        move={move}
+      />
     </div>
   );
 }
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -6,24 +6,49 @@ import styles from './TodoList.module.scss';
 interface Props {
   items: Todo[]
   addTodo: (place: Todo[]) => (title: string) => void;
+  removeTodo: (place: Todo[]) => (id: number) => void;
+  move: (place: Todo[], type: 'up' | 'down') => (index: number) => void;
 }
 
 export const TodoList = memo<Props>(({
   items,
   addTodo,
+  removeTodo,
+  move
 }) => {
   return(
       <ul className={styles.list}>
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li key={item.id}>
             <div className={styles.item}>
               <span>{item.title}</span>
 
               <AddForm addTodo={addTodo(item.items)} />
+
+              <button type="button" onClick={() => removeTodo(items)(item.id)}>
+                Remove
+              </button>
+
+              {index > 0 && (
+                <button type="button" onClick={() => move(items, 'up')(index)}>
+                  Up
+                </button>
+              )}
+
+              {items.length > 1 && index < items.length - 1 && (
+                <button type="button" onClick={() => move(items, 'down')(index)}>
+                  Down
+                </button>
+              )}
             </div>
 
             {Boolean(item.items.length) && (
-              <TodoList items={item.items} addTodo={addTodo}/>
+              <TodoList
+                items={item.items}
+                addTodo={addTodo}
+                removeTodo={removeTodo}
+                move={move}
+              />
             )}
           </li>
         ))}
